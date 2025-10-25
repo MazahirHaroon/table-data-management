7266bfe5f258b140a91d71eae2fd62895aee161a
Mazahir Haroon <mazahirharoon@gmail.com>
2025-10-25 17:27:56 +0530

    build: install latest Tailwind CSS and use it in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <h1> Table Data Management</h1>;
+  return (
+    <header className='flex justify-center gap-2 p-4 m-4'>
+      <h1 className='text-4xl font-bold text-blue-500'> Character's Data</h1>
+    </header>
+  );
 }
 
 export default App;
