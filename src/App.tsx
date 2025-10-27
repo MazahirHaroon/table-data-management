baf941107adde6dbfb3620605846d9daa61d90af
Mazahir Haroon <mazahirharoon@gmail.com>
2025-10-27 14:01:33 +0530

    refactor: move App.tsx to pages directory and fix import paths

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <header className='flex justify-center gap-2 p-4 m-4'>
-      <h1 className='text-4xl font-bold text-blue-500'> Character's Data</h1>
-    </header>
-  );
-}
-
-export default App;
---
+++ src/pages/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Anime</h1>;
+};
+
+export default App;
