0890d076f890ea0965293298a72a8e1f152c0696
Mazahir Haroon <mazahirharoon@gmail.com>
2025-10-28 16:04:27 +0530

    feat: add hideCaption prop to toggle visible caption with screen reader class

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,20 +1,27 @@
 import { type TableIdKey } from '@typesData/characters';
+
 import Header from './header';
 import Row from './row';
 
 interface TableProps<T extends { id: TableIdKey }> {
   caption: string;
+  hideCaption?: boolean;
   headers: Extract<keyof T, string>[];
   rows: T[];
 }
 
 export const Table = <T extends { id: TableIdKey }>({
   caption,
+  hideCaption = false,
   headers,
   rows,
 }: TableProps<T>) => (
   <table className='table-auto border-2 border-table-border'>
-    <caption className='text-secondary-heading text-text-color-subheading font-family-heading font-bold  mb-4'>
+    <caption
+      className={`text-secondary-heading text-text-color-subheading font-family-heading font-bold  mb-4 ${
+        hideCaption ? 'sr-only' : ''
+      }`}
+    >
       {caption}
     </caption>
     <Header headers={headers} />
