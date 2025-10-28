61bd3565745a893eddf9c1913da2ace20fbedf96
Mazahir Haroon <mazahirharoon@gmail.com>
2025-10-28 16:13:03 +0530

    feat: add search bar and submit button above table

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -5,6 +5,8 @@ import type { CharacterList } from '@typesData/characters';
 import { getCharacters } from '@utils/api';
 
 import { Table } from '@components/Table';
+import PrimaryButton from 'src/components/Button/Primary';
+import Input from 'src/components/Input';
 
 const Characters = () => {
   const [characters, setCharacters] = useState<CharacterList[]>([]);
@@ -56,6 +58,18 @@ const Characters = () => {
       <h2 className='text-center text-secondary-heading text-text-color-subheading font-family-heading font-bold  mb-4'>
         Characters' Data
       </h2>
+      <div className='flex justify-between items-center w-full p-2 border-2 border-table-border'>
+        <Input
+          type='text'
+          name='search-characters'
+          label='Search Characters'
+          hideLabel={true}
+          placeholder='Search Characters'
+        />
+        <div className='m-2'>
+          <PrimaryButton content={'Submit'} />
+        </div>
+      </div>
       <Table
         caption="Character's Database"
         hideCaption={true}
