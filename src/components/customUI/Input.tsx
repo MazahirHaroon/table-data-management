469870b4638aa4669d0c1624468647c3a6067ce4
Mazahir Haroon <mazahirharoon@gmail.com>
2025-10-29 11:54:40 +0530

    style: update Input component color classes and add id for accessibility

--- src/components/customUI/Input.tsx
+++ src/components/customUI/Input.tsx
@@ -19,15 +19,16 @@ const Input = ({
   <div className='flex flex-col'>
     <label
       htmlFor={props.name}
-      className={`text-primary-dark font-medium mb-1 ${
+      className={`text-text-color-subheading font-medium mb-1 ${
         hideLabel ? 'sr-only' : ''
       }`}
     >
       {label}
     </label>
     <input
+      id={props.name}
       type={type}
-      className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-primary-dark'
+      className='placeholder-text-color-subheading w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-primary-dark'
       {...props}
     />
     {children}
