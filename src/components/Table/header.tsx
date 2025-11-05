f6c1b6cb6287c7c68a221d7dfec2b674873a3852
Mazahir Haroon <mazahirharoon@gmail.com>
2025-11-06 02:51:12 +0530

    refactor: accept enableSelect and selectColumnLabel props (remove context)

--- src/components/Table/header.tsx
+++ src/components/Table/header.tsx
@@ -1,36 +1,33 @@
-import { useContext, memo } from 'react';
-
-import { TableFeatureContext } from '@context/tableFeatureContext';
+import { memo } from 'react';
 
 interface HeaderProps {
   headers: (string | number | symbol)[];
+  enableSelect?: boolean;
+  selectColumnLabel?: string;
 }
 
-export const Header = ({ headers }: HeaderProps) => {
-  const { selectLabel, toggleSelection } = useContext(TableFeatureContext);
-
-  const showSelect =
-    typeof selectLabel !== 'undefined' || typeof toggleSelection === 'function';
-
-  return (
-    <thead className='sticky top-0'>
-      <tr>
-        {showSelect && (
-          <th className='border-2 border-table-border font-family-body text-text-color-subheading bg-primary-light p-4 w-2 text-center'>
-            {selectLabel}
-          </th>
-        )}
-        {headers.map((header) => (
-          <th
-            key={String(header)}
-            className='border-2 border-table-border font-family-body text-text-color-subheading bg-primary-light p-4 w-48 text-left capitalize'
-          >
-            {header as React.ReactNode}
-          </th>
-        ))}
-      </tr>
-    </thead>
-  );
-};
+export const Header = ({
+  headers,
+  enableSelect = false,
+  selectColumnLabel = 'Select',
+}: HeaderProps) => (
+  <thead className='sticky top-0'>
+    <tr>
+      {enableSelect && (
+        <th className='border-2 border-table-border font-family-body text-text-color-subheading bg-primary-light p-4 w-2 text-center'>
+          {selectColumnLabel}
+        </th>
+      )}
+      {headers.map((header) => (
+        <th
+          key={String(header)}
+          className='border-2 border-table-border font-family-body text-text-color-subheading bg-primary-light p-4 w-48 text-left capitalize'
+        >
+          {header as React.ReactNode}
+        </th>
+      ))}
+    </tr>
+  </thead>
+);
 
 export default memo(Header);
