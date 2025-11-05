43bd55b247823d7a4dc9698fdef9bb9f629a88a1
Mazahir Haroon <mazahirharoon@gmail.com>
2025-11-06 03:37:50 +0530

    fix: add missing type definition file for feature configuration

--- src/typesData/table.ts
+++ src/typesData/table.ts
@@ -1,1 +1,11 @@
+import { FEATURE_SET } from '@constants/table';
+
 export type TableIdKey = string | number;
+
+export type TableFeatures = (typeof FEATURE_SET)[keyof typeof FEATURE_SET];
+
+export type SelectConfig = {
+  columnLabel: string | undefined;
+  buttonLabel: string | undefined;
+  onAction: (selectedIds: TableIdKey[]) => void | Promise<void>;
+};
