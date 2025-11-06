583fdc20be6d84c2e23875886e1660132cab9f67
Mazahir Haroon <mazahirharoon@gmail.com>
2025-11-06 14:56:14 +0530

    refactor: use useFetch hook and remove direct API logic

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,45 +1,29 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useCallback } from 'react';
+
+import type {
+  CharacterList,
+  CharacterApiResponse,
+} from '@typesData/characters';
+import type { TableIdKey } from '@typesData/table';
 
 import { characterTableHeader } from '@constants/characters';
 import { FEATURE_SET } from '@constants/table';
+import { JSON_SERVER } from '@constants/api';
 
-import type { CharacterList } from '@typesData/characters';
-import type { TableIdKey } from '@typesData/table';
-
-import { getCharacters } from '@utils/api';
+import { useFetch } from '@hooks/API';
 
 import { SecondaryHeading, Loading, Error } from '@custom-ui';
 
 import { Table } from '@components/Table';
 
 const Characters = () => {
-  const [characters, setCharacters] = useState<CharacterList[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    const fetchData = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const data = await getCharacters();
-        if (!signal.aborted) {
-          if (data) setCharacters(data);
-          else setError('Failed to load characters.');
-        }
-      } catch (err) {
-        if (!signal.aborted) setError('Failed to load characters.');
-      } finally {
-        if (!signal.aborted) setLoading(false);
-      }
-    };
-
-    fetchData();
-    return () => controller.abort();
-  }, []);
+  const {
+    data: characters,
+    loading,
+    error,
+  } = useFetch<CharacterApiResponse>({
+    url: `${JSON_SERVER}/characters`,
+  });
 
   const handleSelectedAction = useCallback((selectedIds: TableIdKey[]) => {
     console.log('Action for selected IDs:', selectedIds);
