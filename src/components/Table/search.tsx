95a007e4cd32a5e139a64a138867531265f2673d
Mazahir Haroon <mazahirharoon@gmail.com>
2025-11-06 19:45:04 +0530

    style: adjust search input width for better layout alignment

--- src/components/Table/search.tsx
+++ src/components/Table/search.tsx
@@ -16,7 +16,7 @@ export const Search = ({
   name = 'table-search',
 }: Props) => {
   return (
-    <div className=''>
+    <div className='min-w-64'>
       <Input
         type='text'
         name={name}
