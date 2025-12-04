93a94d4478408f0396c50ea1031ea5154cd602e6
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-05 03:28:07 +0530

    feat: add conditional aria-label for hidden labels to improve accessibility

--- src/components/customUI/Checkbox.tsx
+++ src/components/customUI/Checkbox.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
@@ -12,14 +14,16 @@ const CheckBox = ({
   className,
   ...props
 }: CheckboxProps) => {
+  const ariaLabelProps = hideLabel ? { 'aria-label': label } : {};
   return (
     <div className={`flex items-center gap-2 ${className}`}>
       <input
         type='checkbox'
         id={name}
         name={name}
-        {...props}
         className='w-5 h-5 bg-white border-table-border rounded focus:ring-2 focus:ring-primary-dark'
+        {...ariaLabelProps}
+        {...props}
       />
       <label
         htmlFor={name}
